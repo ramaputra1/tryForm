7f0a1379f7236abc953bf51dc56f7c78d56d39e7
ramaputra1 <166339284+ramaputra1@users.noreply.github.com>
2024-11-15 23:05:40 -1000

    ff

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ document.getElementById("myForm").addEventListener("submit", function (event) {
   };
 
   const xhr = new XMLHttpRequest();
-  xhr.open("POST", "submit.json", true);
+  xhr.open("GET", "submit.json", true);
   xhr.setRequestHeader("Content-Type", "application/json; charset=UTF-8");
   xhr.onreadystatechange = function () {
     if (xhr.readyState === 4 && xhr.status === 200) {
