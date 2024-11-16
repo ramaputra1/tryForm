8df647041e241da4e1c0b1679b9de73ce6c02f7a
ramaputra1 <166339284+ramaputra1@users.noreply.github.com>
2024-11-15 22:59:13 -1000

    2

--- script.js
+++ script.js
@@ -25,7 +25,9 @@ document.getElementById("myForm").addEventListener("submit", function (event) {
   xhr.setRequestHeader("Content-Type", "application/json; charset=UTF-8");
   xhr.onreadystatechange = function () {
     if (xhr.readyState === 4 && xhr.status === 200) {
-      alert("Form submitted successfully!");
+      const response = JSON.parse(xhr.responseText);
+      document.getElementById("message").innerHTML = response.messsage;
+      document.getElementById("myForm").innerHTML = "";
     } else if (xhr.readyState === 4) {
       alert("An error occurred while submitting the form.");
     }
